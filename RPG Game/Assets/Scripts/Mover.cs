37f3977f7bd18e133f4bb46078fb7a8cb4a4cdeb
Bukrar <alex968624229@gmail.com>
2021-06-24 17:33:15 +0800

    3.腳色移動動畫、攝影機跟隨

--- RPG Game/Assets/Scripts/Mover.cs
+++ RPG Game/Assets/Scripts/Mover.cs
@@ -15,10 +15,11 @@ public class Mover : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButton(0))
         {
             MoveToCursor();
         }
+        UpdateAnimator();
     }
 
     private void MoveToCursor()
@@ -26,9 +27,17 @@ public class Mover : MonoBehaviour
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
         bool hasHit = Physics.Raycast(ray, out hit);
-        if(hasHit)
+        if (hasHit)
         {
             GetComponent<NavMeshAgent>().destination = hit.point;
         }
+    }
+
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("ForwardSpeed",speed);
     }
 }
